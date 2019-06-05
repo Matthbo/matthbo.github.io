f5ee9e3d3a5ecc497c5a36a8f8f9cc8ee21d9cba
Matthijs Booman <matthijs.booman@hotmail.com>
2019-06-05 21:05:41 +0200

    removed old content, changed homepage

--- grimfight/ownFunctions.js
+++
@@ -1,5 +0,0 @@
-function pointInRectangle(pointX, pointY, rect, size) {
-	return (pointX > rect.x && pointX < rect.x + size &&
-			pointY > rect.y && pointY < rect.y + size)
-}//end of pointInRectangle function
-
